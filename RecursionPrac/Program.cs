c8ab6484b4d554bb1241b3cb24f1804443016f23
Chaeyoung Moon <s2123@e-mirim.hs.kr>
2023-05-25 14:51:55 +0900

    초기 커밋.

---
+++ RecursionPrac/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionPrac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci fibo = new Fibonacci();
+            Console.WriteLine(fibo.Get(10));
+        }
+    }
+}
